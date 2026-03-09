07f140da41d30bfd2c16ffa86f1a19f29cc59983
Erik3331 <erikramon3331@gmail.com>
2026-03-09 10:18:32 -0300

    feat: enhance Sidebar styling with padding, borders, and height adjustments

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { FaHome, FaExclamationCircle, FaPaste, FaCog } from 'react-icons/fa'
 
 export const Sidebar = () => {
   return (
-    <aside className='flex flex-col gap-5 fixed bg-white'>
+    <aside className='flex flex-col pr-10 gap-5 fixed bg-white border-r-2 border-gray-200 h-full w-75'>
       <So to="/" icon={FaHome} text="Início" />
       <So to="/advertencias" icon={FaExclamationCircle} text="Advertências" />
       <So to="/gestao" icon={FaPaste} text="Gestão" />
