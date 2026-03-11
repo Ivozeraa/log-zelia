031016c2d0e9f87c3dd8a45d6dfc584073c3b28c
Erik3331 <erikramon3331@gmail.com>
2026-03-11 11:09:47 -0300

    fix: adjust alignment and spacing in Home component warning section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,9 @@ export const Home = ({ username }) => {
       <p className='text-3xl'>Bem vindo(a), <span className='text-green-700'>{username}</span>!</p>
 
       <div>
-        <span className='flex gap-2 items-center bg-red-900 p-2 rounded-xl cursor-pointer'>
+        <span className='flex gap-2 items-center justify-center bg-red-900 p-2 rounded-xl cursor-pointer'>
           <FaExclamationTriangle size={20} className='text-white'/>
-          <p className='text-white'>Adicionar Advertência</p>
+          <p className='text-white '>Adicionar Advertência</p>
         </span>
       </div>
 
