6101468a255afc70a39860c79ce1b49720679dd2
Erik3331 <erikramon3331@gmail.com>
2026-03-12 07:56:42 -0300

    fix: update Card component layout and styling for improved readability

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 export const Card = ({ title, content, subtitle }) => {
   return (
-    <div className='flex flex-col items-center justify-center w-full bg-white shadow-md rounded-lg p-6 h-60'>
-      <h3 className='text-lg font-bold mb-2'>{title}</h3>
+    <div className='flex flex-col gap-2 items-center justify-center w-full bg-white shadow-md rounded-lg p-6 h-60'>
+      <h3 className='text- font-bold'>{title}</h3>
       <p className='text-6xl font-extrabold text-orange-600'>{content}</p>
-      <p className="text-gray-500">{subtitle}</p>
+      <p className="text-gray-500 text-md">{subtitle}</p>
     </div>
   )
 }
