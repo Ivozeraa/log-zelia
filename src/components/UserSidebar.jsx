165dea22fa7088c69c5df1924f7a3bc82c2d6280
Erik3331 <erikramon3331@gmail.com>
2026-03-14 12:50:56 -0300

    fix: add missing newlines in JSON and JSX files for improved formatting

--- src/components/UserSidebar.jsx
+++ src/components/UserSidebar.jsx
@@ -1,10 +1,20 @@
+import { useAuth } from "../contexts/AuthContext"
+
+import { FaRegUserCircle } from "react-icons/fa"
+
 export const UserSidebar = ({ open }) => {
+
+  const { user } = useAuth()
+
   return (
     <div
-      className={`fixed top-0 right-0 h-screen w-72 bg-white shadow-lg z-50
+      className={`flex flex-col items-center gap-5 p-10 
+      fixed top-0 right-0 h-screen w-72 bg-white shadow-lg z-50
       transform transition-all duration-250 ease-in-out
       ${open ? "translate-x-0" : "translate-x-full"}`}
     >
+      <FaRegUserCircle size={50} />
+      <p className="text-xl font-bold">{user?.nome}</p>
 
     </div>
   )
--- src/pages/Occurrences.jsx
+++ src/pages/Occurrences.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Occurrences = () => {
   return (
     <div>Occurrences</div>
