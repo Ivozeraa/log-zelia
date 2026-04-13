ab89a92c93e163e9038ff4998f7385c3af6cefd9
Ivozeraa <joseivo2008@gmail.com>
2026-04-13 10:10:38 -0300

    refactor: clean up error handling and formatting in uploadAvatar function

--- src/utils/uploadAvatar.js
+++ src/utils/uploadAvatar.js
@@ -3,17 +3,16 @@ import { supabase } from "./supabase";
 export async function uploadAvatar(file) {
   const { data, error } = await supabase.auth.getUser();
 
-  if(error || !data.user || !file){
-    console.error("Usuário não autenticado ou arquivo inválido")
-    return
+  if (error || !data.user || !file) {
+    console.error("Usuário não autenticado ou arquivo inválido");
+    return;
   }
 
-  const user = data.user
-
+  const user = data.user;
+  const ext = file.name.split(".").pop();
   const filePath = `${user.id}/avatar.${ext}`;
 
-  const { error: uploadError } = await supabase
-    .storage
+  const { error: uploadError } = await supabase.storage
     .from("avatars")
     .upload(filePath, file, {
       upsert: true,
@@ -24,16 +23,13 @@ export async function uploadAvatar(file) {
     console.error(uploadError);
     return;
   }
-
   const { data: signedData, error: urlError } = await supabase.storage
     .from("avatars")
     .createSignedUrl(filePath, 60 * 60);
 
-  const publicUrl = publicUrlData?.publicUrl
-
-  if(!publicUrl){
-    console.error("Não foi possível gerar URL pública do avatar")
-    return null
+  if (urlError || !signedData?.signedUrl) {
+    console.error("Erro ao gerar URL:", urlError);
+    return null;
   }
 
   const avatarUrl = signedData.signedUrl;
