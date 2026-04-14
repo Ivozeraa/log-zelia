701f70a68ad153a6585286f2bfa4e83654c259ef
Erik3331 <erikramon3331@gmail.com>
2026-04-14 11:21:10 -0300

    feat: add DivBackground component and integrate it into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,27 @@
+import { useState } from 'react'
 import { FaExclamationTriangle } from 'react-icons/fa'
 import { useAuth } from "../hooks/useAuth"
 import { Card } from '../components/Card'
-import { UploadAvatar } from '../components/UploadAvatar'
-
+import { DivBackground } from '../components/DivBackground'
 
 export const Home = () => {
   const { user } = useAuth()
+  const [open, setOpen] = useState(false)
 
   return (
     <div className='flex flex-col gap-10 w-full'>
-      <p className='text-3xl'>Bem vindo(a), <span className='text-green-700'>{user?.nome}</span>!</p>
+      <p className='text-3xl'>
+        Bem vindo(a), <span className='text-green-700'>{user?.nome}</span>!
+      </p>
 
       <div>
-        <span className='flex w-1/3 gap-2 items-center justify-center bg-red-900 p-2 rounded-xl cursor-pointer'>
+        <button
+          onClick={() => setOpen(!open)}
+          className='flex w-1/3 gap-2 items-center justify-center bg-red-900 p-2 rounded-xl cursor-pointer'
+        >
           <FaExclamationTriangle size={20} className='text-white' />
           <p className='text-white'>Adicionar Advertência</p>
-        </span>
+        </button>
       </div>
 
       <div className='flex flex-col gap-2'>
@@ -26,6 +32,8 @@ export const Home = () => {
           <Card title="Resolvidas" content="135" subtitle="no ano" />
         </div>
       </div>
+
+      {open && <DivBackground close={() => setOpen(false)} />}
     </div>
   )
 }
