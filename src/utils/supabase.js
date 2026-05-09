72760beaf093dda32a3d290fc699e19aa6691783
Ivozeraa <138629986+Ivozeraa@users.noreply.github.com>
2026-05-09 11:34:08 -0300

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const globalKey = "__supabase_client__";
 
+if (!window[globalKey]) {
+  window[globalKey] = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      storageKey: "log-zelia-auth",
+    },
+  });
+}
+
+export const supabase = window[globalKey];
