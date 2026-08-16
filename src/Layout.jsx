c97a8d5eafce9e22d94ac15aafbf934663c06fec
Ivozeraa <138629986+Ivozeraa@users.noreply.github.com>
2026-08-16 18:48:00 -0300

    fix: improve responsive page layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,20 +4,19 @@ import { Sidebar } from './components/layout/Sidebar'
 
 function Layout() {
   return (
-    <div className="font-inter">
-
+    <div className="min-h-screen overflow-x-hidden font-inter bg-neutral-100 dark:bg-gray-950">
       <Header />
 
-      <div className="pt-16 flex">
-
+      <div className="pt-16 flex min-h-screen">
         <aside className="hidden md:block fixed top-16 left-0 w-64 h-[calc(100vh-4rem)] z-40">
           <Sidebar />
         </aside>
 
-        <main className="w-full md:ml-64 p-4 sm:p-6 z-10 relative">
-          <Outlet />
+        <main className="w-full min-w-0 md:ml-64 px-3 py-4 sm:px-5 sm:py-5 lg:px-6 lg:py-6 z-10 relative">
+          <div className="mx-auto w-full max-w-[1600px] min-w-0">
+            <Outlet />
+          </div>
         </main>
-
       </div>
     </div>
   )
